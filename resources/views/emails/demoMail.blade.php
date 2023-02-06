f80869897d4937040b9853a68fc899d82e29edeb
atifnaz <atifnaz667@gmail.com>
2023-02-06 15:29:52 +0500

    update

---
+++ resources/views/emails/demoMail.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Dynamic Email</title>
+</head>
+
+<body>
+    <h1>{{ $mailData['title'] }}</h1>
+    <p>{{ $mailData['body'] }}</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+
+    <p>Thank you</p>
+</body>
+
+</html>
